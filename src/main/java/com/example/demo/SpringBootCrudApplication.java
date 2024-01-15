62d13875539a95dfb87eb078753d03fd1ccd480c
joy539men <joy539men@gmail.com>
2024-01-15 15:36:19 +0800

    0115

--- src/main/java/com/example/demo/SpringBootCrudApplication.java
+++ src/main/java/com/example/demo/SpringBootCrudApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootCrudApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SpringBootCrudApplication.class, args);
 	}
 
