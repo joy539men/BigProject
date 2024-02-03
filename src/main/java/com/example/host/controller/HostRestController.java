961f8d1cfa55efba4bec53da9cd59c64392c7036
nnn51 <ceannn51@gmail.com>
2024-02-03 18:15:21 +0800

    update bookingBean method keyword

--- src/main/java/com/example/host/controller/HostRestController.java
+++ src/main/java/com/example/host/controller/HostRestController.java
@@ -8,8 +8,6 @@ import java.util.Set;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -46,7 +44,6 @@ public class HostRestController {
 		this.session = session;
 	}
 	
-	private static final Logger logger = LoggerFactory.getLogger(HostRestController.class);
 	
 	//傳送新增房間請求
 	@PostMapping("/addRoom")
